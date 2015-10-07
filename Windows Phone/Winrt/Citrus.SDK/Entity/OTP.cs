5bb2019edbcd1ad5f9fca41e1d0e4f0b79f7f7c3
MaheshMutyal <mahesh.mutyal@citruspay.com>
2015-10-07 10:46:36 +0530

    new entity

---
+++ Windows Phone/Winrt/Citrus.SDK/Entity/OTP.cs
@@ -0,0 +1,45 @@
+﻿namespace Citrus.SDK.Entity
+{
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+
+    public class OTP : IEntity
+    {
+        #region Public Properties
+
+        [JsonProperty("source")]
+        public string Source { get; set; }
+
+        [JsonProperty("otpType")]
+        public string OTPType { get; set; }
+
+        [JsonProperty("identity")]
+        public string Identity { get; set; }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Return the key value pair of properties
+        /// </summary>
+        /// <returns>
+        /// Key value pair of properties to be posted
+        /// </returns>
+        public IEnumerable<KeyValuePair<string, string>> ToKeyValuePair()
+        {
+            return new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("source", this.Source), 
+                    new KeyValuePair<string, string>(
+                        "otpType", 
+                        this.OTPType), 
+                    new KeyValuePair<string, string>(
+                        "identity", 
+                        this.Identity)
+                };
+        }
+
+        #endregion
+    }
+}
---
+++ Windows Phone/Winrt/Citrus.SDK/Entity/UserProfile.cs
@@ -0,0 +1,85 @@
+﻿namespace Citrus.SDK.Entity
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+
+    public class UserProfile : IEntity
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("emailVerified")]
+        public string EmailVerified { get; set; }
+
+        [JsonProperty("emailVerifiedDate")]
+        public string EmailVerifiedDate { get; set; }
+
+        [JsonProperty("mobile")]
+        public string Mobile { get; set; }
+
+        [JsonProperty("mobileVerified")]
+        public string MobileVerified { get; set; }
+
+        [JsonProperty("mobileVerifiedDate")]
+        public string MobileVerifiedDate { get; set; }
+
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("uuid")]
+        public string UUID { get; set; }
+
+        public IEnumerable<KeyValuePair<string, string>> ToKeyValuePair()
+        {
+            return new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("email", this.Email), 
+                    new KeyValuePair<string, string>(
+                        "firstName", 
+                        this.FirstName), 
+                    new KeyValuePair<string, string>(
+                        "lastName", 
+                        this.LastName), 
+                    new KeyValuePair<string, string>(
+                        "mobile", 
+                        this.Mobile)
+                };
+        }
+    }
+
+    public class UserProfileRequest : IEntity
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("mobile")]
+        public string Mobile { get; set; }
+
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        public IEnumerable<KeyValuePair<string, string>> ToKeyValuePair()
+        {
+            return new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("email", this.Email), 
+                    new KeyValuePair<string, string>(
+                        "firstName", 
+                        this.FirstName), 
+                    new KeyValuePair<string, string>(
+                        "lastName", 
+                        this.LastName), 
+                    new KeyValuePair<string, string>(
+                        "mobile", 
+                        this.Mobile)
+                };
+        }
+    }
+}
